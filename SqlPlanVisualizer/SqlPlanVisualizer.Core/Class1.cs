1c2f4dd4e7c05a83424bfb64420f633756cd5f94
Juhi111 <juhad222@gmail.com>
2026-09-16 22:25:32 +0200

    alkalmazás szerkezeti alapok

---
+++ SqlPlanVisualizer/SqlPlanVisualizer.Core/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace SqlPlanVisualizer.Core
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ SqlPlanVisualizer/SqlPlanVisualizer.Infrastructure/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace SqlPlanVisualizer.Infrastructure
+{
+    public class Class1
+    {
+
+    }
+}
